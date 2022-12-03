c6bde70ae8d8b21e55274bebb0f2f45d9a7326dd
Dakota Greenlee <dg2120032@gmail.com>
2022-12-03 10:07:05 -0800

    added variables to js file

---
+++ app.js
@@ -0,0 +1,8 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
+
+let result = 0
+
+//function to randomly select a square on the grid
